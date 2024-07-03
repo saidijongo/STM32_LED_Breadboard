2bc70f6fe146ee4856ddf87be00f95c658c45ddd
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-07-03 17:18:00 +0900

    Create pin_mappings.h

---
+++ pin_mappings.h
@@ -0,0 +1,28 @@
+/*
+ * pin_mappings.h
+ *
+ *  Created on: Jul 3, 2024
+ *      Author: Jongo
+ */
+
+#ifndef INC_PIN_MAPPINGS_H_
+#define INC_PIN_MAPPINGS_H_
+
+// Define GPIO ports and pins
+#define LED1_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_6
+
+#define LED2_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_11
+
+#define LED3_GPIO_Port GPIOC
+#define LED3_Pin GPIO_PIN_8
+
+#define LED4_GPIO_Port GPIOB
+#define LED4_Pin GPIO_PIN_0
+
+#define LED5_GPIO_Port GPIOA
+#define LED5_Pin GPIO_PIN_5
+
+
+#endif /* INC_PIN_MAPPINGS_H_ */
